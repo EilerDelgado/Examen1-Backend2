0e18c659c0f28842ee1aefa921c34d7fefe3e83f
EilerDelgado <eilernaindt.16@gmail.com>
2025-08-21 18:05:02 -0500

    Segundo commit: Se corregi todos los errores de la entidad curso

--- src/main/java/com/example/Examen1Back2/modelos/Curso.java
+++ src/main/java/com/example/Examen1Back2/modelos/Curso.java
@@ -1,27 +1,54 @@
 package com.example.Examen1Back2.modelos;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+
 @Entity
+@Table(name = "cursos")
 public class Curso {
 
-    @I
-    @Ge(strategy = IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
-    private String nombre
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
 
     @ManyToOne
-    @JoinColumn(name="fk_docente", referencedColumnName = "id");
+    @JoinColumn(name= "fk_docente", referencedColumnName = "id")
     @JsonBackReference(value = "docente-curso")
-    Docente docente
+    private Docente docente;
 
     public Curso() {
     }
 
-    public Curso(Integer id, String nombre) {
+    public Curso(Integer id, String nombre, Docente docente) {
         this.id = id;
+        this.nombre = nombre;
+        this.docente = docente;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    public Docente getDocente() {
+        return docente;
+    }
 
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
 }
